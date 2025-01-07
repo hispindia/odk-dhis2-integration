195a3fe1954130fed43bae4517a784db48ae829e
sourabhB <sourabh.bhardwaj@hispindia.org>
2025-01-07 14:14:37 +0530

    Add constants for hr domain including API URLs and authentication

--- constants.py
+++ constants.py
@@ -7,13 +7,13 @@ if os.path.exists(auth_file):
 else:
     raise FileNotFoundError("Authentication file not found!")
 
-ODK_API_URL = "https://iphs.abdm.gov.in/v1/projects/1/forms/iphs_database.svc/Submissions?$filter=__system/submissionDate ge 2025-01-05&$skip=10&$top=2"
+ODK_API_URL = "https://iphs.abdm.gov.in/v1/projects/1/forms/iphs_database.svc/Submissions?$filter=__system/submissionDate ge 2025-01-05&$skip=10&$top=1"
 ODK_AUTH = (auth_data.get("ODK_USERNAME"), auth_data.get("ODK_PASSWORD"))
 
 DHIS2_API_URL = "https://iphs.nipi-cure.org/api"
 DHIS2_AUTH = (auth_data.get("DHIS2_USERNAME"), auth_data.get("DHIS2_PASSWORD"))
 
-LOG_FILE = "gov_data_push.log"
-PROGRAM_STAGE_ID = "ZJ6HL7aOf8X"
+LOG_FILE = "hr_data_push.log"
+PROGRAM_STAGE_ID = "hiF9ywHeAlE"
 TRACKED_ENTITY_TYPE = "tbqOAw2BJIe"
-PROGRAM_ID = "bV4VOVumELH"
+PROGRAM_ID = "xHqM68DpuZj"
