3bcd2a2f573b3f743a937ee9db4cf2e82aa8459e
sourabhB <sourabh.bhardwaj@hispindia.org>
2025-01-07 14:15:07 +0530

    Implement mapping logic to convert ODK data to DHIS2-compatible format

--- mapping.py
+++ mapping.py
@@ -1,24 +1,12 @@
-def map_odk_to_dhis2(odk_data, calculate_element_mapping, non_calculate_element_mapping):
+def map_odk_to_dhis2(odk_data, data_element_mapping):
     data_values = []
 
-    # Process calculate elements
     for key, value in odk_data.items():
-        if key in calculate_element_mapping and value is not None:
+        if key in data_element_mapping and value is not None:
+            element_details = data_element_mapping[key]
             data_values.append({
-                "dataElement": calculate_element_mapping[key],
+                "dataElement": element_details["id"],
                 "value": value
             })
-
-    # Process non-calculate elements
-    for key, value in odk_data.items():
-        if key in non_calculate_element_mapping and value is not None:
-            values = str(value).split()
-            for v in values:
-                full_code = f"{key}_{v}"
-                if full_code in non_calculate_element_mapping:
-                    data_values.append({
-                        "dataElement": non_calculate_element_mapping[full_code],
-                        "value": v
-                    })
 
     return data_values
